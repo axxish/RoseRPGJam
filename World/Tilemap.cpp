96bac5c0cace21f967d06e4535565e384e479c10
axxy <local>
2024-10-31 21:50:26 +0200

    filename change

---
+++ World/Tilemap.cpp
@@ -0,0 +1,11 @@
+#include "Tilemap.h"
+Tilemap::Tilemap( uint16_t width, uint16_t height)
+    : p_width(width), p_height(height)
+{
+    auto size = p_width * p_height;
+    tiles.reserve(p_width * p_height);
+    for(int i = 0; i < size; i++){
+        tiles.push_back(0);
+    }
+}
+
